c330f6ddb53962099e251e4ee2c9d7d41ce8b969
Sumit Khanna <sumit.khanna@gettyimages.cm>
2016-04-18 16:43:37 -0700

    started tagister subproject

---
+++ tagister/TagBrowser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Gtk;
+
+public class TagBrowser: Gtk.Window {
+  
+  public TagBrowser() : base(Gtk.WindowType.Toplevel) {
+
+    //var splitPanel = new Gtk.Paned();
+
+    //splitPanel.Add1(new Label("File List Place Holder"));
+
+    var grid = new Gtk.Table(5, 6, true);
+
+  }
+
+  protected void OnDeleteEvent(object sender, DeleteEventArgs a) {
+    Application.Quit();
+    a.RetVal = true;
+  }
+}
---
+++ tagister/Tagster.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Gtk;
+
+namespace tagister {
+  class Tagster {
+    public static void Main(string[] args) {
+      Application.Init();
+      TagBrowser win = new TagBrowser();
+      win.Show();
+      Application.Run();
+    }
+  }
+}
