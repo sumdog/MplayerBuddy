c5f4c4b4ab7ba5f753f37445343931adbf06dca4
Sumit Khanna <sumit.khanna@gettyimages.cm>
2016-04-21 16:46:02 -0700

    DB schema

--- tagister/TagDB.cs
+++ tagister/TagDB.cs
@@ -11,15 +11,22 @@ namespace tagister {
 
     private IDbConnection conn;
 
+    private void RunSQL(string sql) {
+      var cmd = conn.CreateCommand();
+      cmd.CommandText = sql;
+      cmd.ExecuteReader();
+      cmd.Dispose();
+    }
+
     public TagDB() {
       if(!File.Exists(dbFile)) {
         SqliteConnection.CreateFile(dbFile);
       }
       conn = new SqliteConnection(String.Format("URI=file:{0}", dbFile));
       conn.Open();
-      var cmd = conn.CreateCommand();
-      cmd.CommandText = "CREATE TABLE IF NOT EXISTS tags(id INT PRIMARY KEY NOT NULL, tag TEXT)";
-      cmd.ExecuteReader();
+      RunSQL("CREATE TABLE IF NOT EXISTS tags(id INT PRIMARY KEY NOT NULL, tag TEXT)");
+      RunSQL("CREATE TABLE IF NOT EXISTS files(id INT PRIMARY KEY NOT NULL, name TEXT)");
+      RunSQL("CREATE TABLE IF NOT EXISTS filetags(file_id INT, tag_id INT)");
     }
 
   }
