960e5296b2b4cda19829439871f752a7adb32b1b
Sumit Khanna <sumit@penguindreams.org>
2016-04-22 08:35:24 -0700

    starterd sqlite reader functions

--- tagister/TagDB.cs
+++ tagister/TagDB.cs
@@ -6,27 +6,40 @@ using Mono.Data.Sqlite;
 namespace tagister {
   
   public class TagDB {
-  
-    const string dbFile = "/home/skhanna/.tagster/tagster.sqlite3";
+
+    string Home = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
 
     private IDbConnection conn;
 
     private void RunSQL(string sql) {
       var cmd = conn.CreateCommand();
       cmd.CommandText = sql;
-      cmd.ExecuteReader();
+      var r = cmd.ExecuteReader();
       cmd.Dispose();
+      //return r;
     }
 
     public TagDB() {
+
+      var dbFile = Path.Combine(Home, ".tagster/tagster.sqlite3");
+
       if(!File.Exists(dbFile)) {
         SqliteConnection.CreateFile(dbFile);
       }
-      conn = new SqliteConnection(String.Format("URI=file:{0}", dbFile));
-      conn.Open();
-      RunSQL("CREATE TABLE IF NOT EXISTS tags(id INT PRIMARY KEY NOT NULL, tag TEXT)");
-      RunSQL("CREATE TABLE IF NOT EXISTS files(id INT PRIMARY KEY NOT NULL, name TEXT)");
-      RunSQL("CREATE TABLE IF NOT EXISTS filetags(file_id INT, tag_id INT)");
+      using(conn = new SqliteConnection(String.Format("URI=file:{0}", dbFile))){;
+        conn.Open();
+        RunSQL("CREATE TABLE IF NOT EXISTS tags(id INT PRIMARY KEY NOT NULL, tag TEXT)");
+        RunSQL("CREATE TABLE IF NOT EXISTS files(id INT PRIMARY KEY NOT NULL, name TEXT)");
+        RunSQL("CREATE TABLE IF NOT EXISTS filetags(file_id INT, tag_id INT)");
+        conn.Close();
+      }
+    }
+
+    public String[] Tags {
+      get {
+        RunSQL("SELECT * FROM tags"); 
+        return new String[] { "", "" };
+      }
     }
 
   }
