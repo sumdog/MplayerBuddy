a46c549de688e15492693a02101dbe1dafcb6beb
Sumit Khanna <sumit@penguindreams.org>
2016-06-21 21:51:59 -0700

    added more passing tests

--- tagister/TagTBTests.cs
+++ tagister/TagTBTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using NUnit.Framework;
+using System.Linq;
 
 namespace tagster {
 
@@ -25,10 +26,24 @@ namespace tagster {
     }
 
     [Test]
-    public void AddFile() {
+    public void AddFiles() {
       database.AddFile("foo.mp4");
+      database.AddFile("bar.mp4");
+      database.AddFile("foobar.mp4");
       database.ListFiles();
-      Assert.That(database.ListFiles().Count == 2);
+      Assert.That(database.ListFiles().Count == 3);
+    }
+
+    [Test]
+    public void AddTags() {
+      database.AddTag("tagA");
+      database.AddTag("tabB");
+      database.AddTag("tagC");
+      database.AddTag("tagD");
+      Assert.That(database.Tags.Count == 4);
+      foreach(var v in database.TagUsage()) {
+        Assert.That(v.Value == 0);
+      }
     }
 
   }
